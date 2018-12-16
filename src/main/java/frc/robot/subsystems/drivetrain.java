d67dc9cc7ee5f6005d15f7f604a16d384ca199ac
mcm001 <matthew.morley.ca@gmail.com>
2018-12-16 01:30:59 -0800

    drivetrain: add mag encoders, support seperate shifting command

--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -2,12 +2,14 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.sun.java.swing.plaf.windows.TMSchema.Control;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.Robot;
+// import frc.robot.Robot;
 import frc.robot.robotconfig;
+import frc.robot.commands.*;
+
 
 /**
  * Drivetrain subsystem. Initilizes the 4 drivetrain talons based on robotmap settings
@@ -16,52 +18,28 @@ public class drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-    TalonSRX m_left_talon = new TalonSRX(robotconfig.m_left_talon_port);
-    TalonSRX s_left_talon = new TalonSRX(robotconfig.s_left_talon_port);
-    TalonSRX m_right_talon = new TalonSRX(robotconfig.m_right_talon_port);
-    TalonSRX s_right_talon = new TalonSRX(robotconfig.s_right_talon_port);
-    DoubleSolenoid shifter_solenoid = new DoubleSolenoid(robotconfig.drivetrain_solenoid_low_gear_channel, robotconfig.drivetrain_solenoid_high_gear_channel);
+    public TalonSRX m_left_talon = new TalonSRX(robotconfig.m_left_talon_port);
+    public TalonSRX s_left_talon = new TalonSRX(robotconfig.s_left_talon_port);
+    public TalonSRX m_right_talon = new TalonSRX(robotconfig.m_right_talon_port);
+    public TalonSRX s_right_talon = new TalonSRX(robotconfig.s_right_talon_port);
+    public DoubleSolenoid shifter_solenoid = new DoubleSolenoid(robotconfig.drivetrain_solenoid_low_gear_channel, robotconfig.drivetrain_solenoid_high_gear_channel);
     
+
     public void init() {
       s_left_talon.set(ControlMode.Follower, m_left_talon.getDeviceID());
       s_right_talon.set(ControlMode.Follower, m_right_talon.getDeviceID());
       if ( robotconfig.drivetrain_starting_gear == "low" ) {
-        setLowGear();
+        new drivetrain_shift_low();
       }
-      else if ( robotconfig.drivetrain_starting_gear == "high" ) {
-        setHighGear();
+      if ( robotconfig.drivetrain_starting_gear == "high" ) {
+        new drivetrain_shift_high();
       }
+
+      m_left_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,0); // TODO put encoder stats on smartdashboard
+      m_right_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,0); // TODO put encoder stats on smartdashboard
+      // TODO do the encoders need to be reversed?
+
     }
-
-
-  public void setLowGear() {
-    m_left_talon.config_kP(0, robotconfig.m_left_velocity_kp_low, 0);
-    m_left_talon.config_kI(0, robotconfig.m_left_velocity_ki_low, 0);
-    m_left_talon.config_kD(0, robotconfig.m_left_velocity_kd_low, 0);
-    m_left_talon.config_kF(0, robotconfig.m_left_velocity_kf_low, 0);
-    
-    m_right_talon.config_kP(0, robotconfig.m_right_velocity_kp_low, 0);
-    m_right_talon.config_kI(0, robotconfig.m_right_velocity_ki_low, 0);
-    m_right_talon.config_kD(0, robotconfig.m_right_velocity_kd_low, 0);
-    m_right_talon.config_kF(0, robotconfig.m_right_velocity_kf_low, 0);
-    shifter_solenoid.set(DoubleSolenoid.Value.kForward);
-    // TODO verify that kForward is low gear
-
-  }
-  public void setHighGear() {
-    m_left_talon.config_kP(0, robotconfig.m_left_velocity_kp_high, 0);
-    m_left_talon.config_kI(0, robotconfig.m_left_velocity_ki_high, 0);
-    m_left_talon.config_kD(0, robotconfig.m_left_velocity_kd_high, 0);
-    m_left_talon.config_kF(0, robotconfig.m_left_velocity_kf_high, 0);
-    
-    m_right_talon.config_kP(0, robotconfig.m_right_velocity_kp_high, 0);
-    m_right_talon.config_kI(0, robotconfig.m_right_velocity_ki_high, 0);
-    m_right_talon.config_kD(0, robotconfig.m_right_velocity_kd_high, 0);
-    m_right_talon.config_kF(0, robotconfig.m_right_velocity_kf_high, 0);
-    shifter_solenoid.set(DoubleSolenoid.Value.kReverse);
-    // TODO verify that kForward is high gear
-
-  }
 
   public void arcade(double forwardspeed, double turnspeed) {
     double leftspeed = forwardspeed + turnspeed;
