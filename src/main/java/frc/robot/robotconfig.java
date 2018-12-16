861763f8d0c59a34d9c8a5ba4a225b5c542a7e60
mcm001 <matthew.morley.ca@gmail.com>
2018-12-15 23:25:19 -0800

    add low and high gears to drivetrain.java

--- src/main/java/frc/robot/robotconfig.java
+++ src/main/java/frc/robot/robotconfig.java
@@ -1,23 +1,57 @@
 package frc.robot;
 public class robotconfig {
 
-/**
- * TalonSRX drivetrain configuration
- */
+public static final int drivetrain_solenoid_low_gear_channel = 1;
+public static final int drivetrain_solenoid_high_gear_channel = 2;
+
+
+
 public static final int m_left_talon_port = 1;
 public static final int s_left_talon_port = 2;
 public static final boolean m_left_inverted = false;
-public static final double m_left_low_kp = 1;
-public static final double m_left_low_ki = 0;
-public static final double m_left_low_kd = 0;
+
+public static final double m_left_velocity_kp_low = 1;
+public static final double m_left_velocity_ki_low = 0;
+public static final double m_left_velocity_kd_low = 0;
+public static final double m_left_velocity_kf_low = 0;
+
+public static final double m_left_position_kp_low = 1;
+public static final double m_left_position_ki_low = 1;
+public static final double m_left_position_kd_low = 1;
+public static final double m_left_position_kf_low = 1;
+
+public static final double m_left_velocity_kp_high = 1;
+public static final double m_left_velocity_ki_high = 0;
+public static final double m_left_velocity_kd_high = 0;
+public static final double m_left_velocity_kf_high = 0;
+
+public static final double m_left_position_kp_high = 1;
+public static final double m_left_position_ki_high = 1;
+public static final double m_left_position_kd_high = 1;
+public static final double m_left_position_kf_high = 1;
 
 public static final int m_right_talon_port = 3;
 public static final int s_right_talon_port = 4;
 public static final boolean m_right_inverted = false;
-public static final double m_right_low_kp = 1;
-public static final double m_right_low_ki = 0;
-public static final double m_right_low_kd = 0;
 
+public static final double m_right_velocity_kp_low = m_left_velocity_kp_low;
+public static final double m_right_velocity_ki_low = m_left_velocity_ki_low;
+public static final double m_right_velocity_kd_low = m_left_velocity_kd_low;
+public static final double m_right_velocity_kf_low = m_left_velocity_kf_low;
 
+public static final double m_right_position_kp_low = m_left_position_kp_low;
+public static final double m_right_position_ki_low = m_left_position_ki_low;
+public static final double m_right_position_kd_low = m_left_position_kd_low;
+public static final double m_right_position_kf_low = m_left_position_kf_low;
+
+public static final double m_right_velocity_kp_high = m_left_velocity_kp_high;
+public static final double m_right_velocity_ki_high = m_left_velocity_ki_high;
+public static final double m_right_velocity_kd_high = m_left_velocity_kd_high;
+public static final double m_right_velocity_kf_high = m_left_velocity_kf_high;
+
+public static final double m_right_position_kp_high = m_left_position_kp_high;
+public static final double m_right_position_ki_high = m_left_position_ki_high;
+public static final double m_right_position_kd_high = m_left_position_kd_high;
+public static final double m_right_position_kf_high = m_left_position_kf_high;
 
 }
--- src/main/java/frc/robot/subsystems/drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Robot;
 import frc.robot.robotconfig;
@@ -12,17 +13,44 @@ import frc.robot.robotconfig;
 public class drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  void init() {
-    TalonSRX m_left = new TalonSRX(robotconfig.m_left_talon_port);
+
+    TalonSRX m_left_talon = new TalonSRX(robotconfig.m_left_talon_port);
+    TalonSRX s_left_talon = new TalonSRX(robotconfig.s_left_talon_port);
+    TalonSRX m_right_talon = new TalonSRX(robotconfig.m_right_talon_port);
+    TalonSRX s_right_talon = new TalonSRX(robotconfig.s_right_talon_port);
+    DoubleSolenoid shifter_solenoid = new DoubleSolenoid(robotconfig.drivetrain_solenoid_low_gear_channel, robotconfig.drivetrain_solenoid_high_gear_channel);
 
 
+  void setLowGear() {
+    m_left_talon.config_kP(0, robotconfig.m_left_velocity_kp_low, 0);
+    m_left_talon.config_kI(0, robotconfig.m_left_velocity_ki_low, 0);
+    m_left_talon.config_kD(0, robotconfig.m_left_velocity_kd_low, 0);
+    m_left_talon.config_kF(0, robotconfig.m_left_velocity_kf_low, 0);
     
-    TalonSRX m_right = new TalonSRX(robotconfig.m_right_talon_port);
-  
-  
-  
-  
+    m_right_talon.config_kP(0, robotconfig.m_right_velocity_kp_low, 0);
+    m_right_talon.config_kI(0, robotconfig.m_right_velocity_ki_low, 0);
+    m_right_talon.config_kD(0, robotconfig.m_right_velocity_kd_low, 0);
+    m_right_talon.config_kF(0, robotconfig.m_right_velocity_kf_low, 0);
+    shifter_solenoid.set(DoubleSolenoid.Value.kForward);
+    // TODO verify that kForward is low gear
+
   }
+  void setHighGear() {
+    m_left_talon.config_kP(0, robotconfig.m_left_velocity_kp_high, 0);
+    m_left_talon.config_kI(0, robotconfig.m_left_velocity_ki_high, 0);
+    m_left_talon.config_kD(0, robotconfig.m_left_velocity_kd_high, 0);
+    m_left_talon.config_kF(0, robotconfig.m_left_velocity_kf_high, 0);
+    
+    m_right_talon.config_kP(0, robotconfig.m_right_velocity_kp_high, 0);
+    m_right_talon.config_kI(0, robotconfig.m_right_velocity_ki_high, 0);
+    m_right_talon.config_kD(0, robotconfig.m_right_velocity_kd_high, 0);
+    m_right_talon.config_kF(0, robotconfig.m_right_velocity_kf_high, 0);
+    shifter_solenoid.set(DoubleSolenoid.Value.kReverse);
+    // TODO verify that kForward is high gear
+
+  }
+
+
 
 
   @Override
