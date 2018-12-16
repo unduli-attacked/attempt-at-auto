e8a66df526ac9f58e2b38ad1a36babbfcf82d184
mcm001 <matthew.morley.ca@gmail.com>
2018-12-16 01:15:34 -0800

    robotconfig.java: add shifting buttons to support addition of shifting

--- src/main/java/frc/robot/robotconfig.java
+++ src/main/java/frc/robot/robotconfig.java
@@ -1,68 +1,71 @@
 package frc.robot;
 public class robotconfig {
 
-public static final int primary_joystick_port = 0;
-public static final int forward_axis = 0;
-public static final int turn_axis = 1;
-public static final int secondary_joystick_port = 1;
+  public static final int primary_joystick_port = 0;
+  public static final int forward_axis = 0;
+  public static final int turn_axis = 1;
+  public static final int shift_down_button = xboxmap.Buttons.LB_BUTTON;
+  public static final int shift_up_button = xboxmap.Buttons.RB_BUTTON;
 
 
+  public static final int secondary_joystick_port = 1;
 
-public static final int drivetrain_solenoid_low_gear_channel = 1;
-public static final int drivetrain_solenoid_high_gear_channel = 2;
-  /**
-   * This function is called periodically during operator control.
-   * The values are low or high (no caps)
-   */
-public static final String drivetrain_starting_gear = "low";
+  // TalonSRX configuration
+  public static final int drivetrain_solenoid_low_gear_channel = 1;
+  public static final int drivetrain_solenoid_high_gear_channel = 2;
+    /**
+     * This function is called periodically during operator control.
+     * The values are low or high (no caps)
+     */
+  public static final String drivetrain_starting_gear = "low";
 
-public static final int m_left_talon_port = 1;
-public static final int s_left_talon_port = 2;
-public static final boolean m_left_inverted = false;
+  public static final int m_left_talon_port = 1;
+  public static final int s_left_talon_port = 2;
+  public static final boolean m_left_inverted = false;
 
-public static final double m_left_velocity_kp_low = 1;
-public static final double m_left_velocity_ki_low = 0;
-public static final double m_left_velocity_kd_low = 0;
-public static final double m_left_velocity_kf_low = 0;
+  public static final double m_left_velocity_kp_low = 1;
+  public static final double m_left_velocity_ki_low = 0;
+  public static final double m_left_velocity_kd_low = 0;
+  public static final double m_left_velocity_kf_low = 0;
 
-public static final double m_left_position_kp_low = 1;
-public static final double m_left_position_ki_low = 1;
-public static final double m_left_position_kd_low = 1;
-public static final double m_left_position_kf_low = 1;
+  public static final double m_left_position_kp_low = 1;
+  public static final double m_left_position_ki_low = 1;
+  public static final double m_left_position_kd_low = 1;
+  public static final double m_left_position_kf_low = 1;
 
-public static final double m_left_velocity_kp_high = 1;
-public static final double m_left_velocity_ki_high = 0;
-public static final double m_left_velocity_kd_high = 0;
-public static final double m_left_velocity_kf_high = 0;
+  public static final double m_left_velocity_kp_high = 1;
+  public static final double m_left_velocity_ki_high = 0;
+  public static final double m_left_velocity_kd_high = 0;
+  public static final double m_left_velocity_kf_high = 0;
 
-public static final double m_left_position_kp_high = 1;
-public static final double m_left_position_ki_high = 1;
-public static final double m_left_position_kd_high = 1;
-public static final double m_left_position_kf_high = 1;
+  public static final double m_left_position_kp_high = 1;
+  public static final double m_left_position_ki_high = 1;
+  public static final double m_left_position_kd_high = 1;
+  public static final double m_left_position_kf_high = 1;
 
-public static final int m_right_talon_port = 3;
-public static final int s_right_talon_port = 4;
-public static final boolean m_right_inverted = false;
+  public static final int m_right_talon_port = 3;
+  public static final int s_right_talon_port = 4;
+  public static final boolean m_right_inverted = false;
 
-public static final double m_right_velocity_kp_low = m_left_velocity_kp_low;
-public static final double m_right_velocity_ki_low = m_left_velocity_ki_low;
-public static final double m_right_velocity_kd_low = m_left_velocity_kd_low;
-public static final double m_right_velocity_kf_low = m_left_velocity_kf_low;
+  public static final double m_right_velocity_kp_low = m_left_velocity_kp_low;
+  public static final double m_right_velocity_ki_low = m_left_velocity_ki_low;
+  public static final double m_right_velocity_kd_low = m_left_velocity_kd_low;
+  public static final double m_right_velocity_kf_low = m_left_velocity_kf_low;
 
-public static final double m_right_position_kp_low = m_left_position_kp_low;
-public static final double m_right_position_ki_low = m_left_position_ki_low;
-public static final double m_right_position_kd_low = m_left_position_kd_low;
-public static final double m_right_position_kf_low = m_left_position_kf_low;
+  public static final double m_right_position_kp_low = m_left_position_kp_low;
+  public static final double m_right_position_ki_low = m_left_position_ki_low;
+  public static final double m_right_position_kd_low = m_left_position_kd_low;
+  public static final double m_right_position_kf_low = m_left_position_kf_low;
 
-public static final double m_right_velocity_kp_high = m_left_velocity_kp_high;
-public static final double m_right_velocity_ki_high = m_left_velocity_ki_high;
-public static final double m_right_velocity_kd_high = m_left_velocity_kd_high;
-public static final double m_right_velocity_kf_high = m_left_velocity_kf_high;
+  public static final double m_right_velocity_kp_high = m_left_velocity_kp_high;
+  public static final double m_right_velocity_ki_high = m_left_velocity_ki_high;
+  public static final double m_right_velocity_kd_high = m_left_velocity_kd_high;
+  public static final double m_right_velocity_kf_high = m_left_velocity_kf_high;
 
-public static final double m_right_position_kp_high = m_left_position_kp_high;
-public static final double m_right_position_ki_high = m_left_position_ki_high;
-public static final double m_right_position_kd_high = m_left_position_kd_high;
-public static final double m_right_position_kf_high = m_left_position_kf_high;
+  public static final double m_right_position_kp_high = m_left_position_kp_high;
+  public static final double m_right_position_ki_high = m_left_position_ki_high;
+  public static final double m_right_position_kd_high = m_left_position_kd_high;
+  public static final double m_right_position_kf_high = m_left_position_kf_high;
 
 
 
