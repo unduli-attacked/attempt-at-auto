b70f490eb68e3716a83cdd2168c0c2f74fba860f
mcm001 <matthew.morley.ca@gmail.com>
2018-12-19 22:23:23 -0800

    Robot.java: reorganization, cleanup of code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,12 +12,15 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.auto.auto_action_DRIVE;
 import frc.robot.commands.arcade_drive;
 // import frc.robot.commands.drivetrain_shift_high;
 // import frc.robot.commands.drivetrain_shift_low;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 // import frc.robot.commands.arcade_drive;
@@ -26,33 +29,38 @@ import frc.robot.subsystems.*;
 
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
+ * 5940 Offseason Command codebase. Currently very much a WIP, but funcitonal. Teleop feature parity with 2018-codebase
+ * @author Matthew Morley
  */
 public class Robot extends TimedRobot {
   public static boolean arcade_running = false;
-  public static final drivetrain drivetrain  = new drivetrain();
-  public static final intake intake = new intake();
-  public static final elevator elevator = new elevator();
-  public static final wrist wrist = new wrist();
-  public static final robotconfig robotconfig = new robotconfig();
+  public static drivetrain drivetrain  = new drivetrain();
+  public static intake intake = new intake();
+  public static elevator elevator = new elevator();
+  public static wrist wrist = new wrist();
+  public static robotconfig robotconfig = new robotconfig();
   public static OI m_oi;
 
-  public static double elevator_setpoint = 0;
-  public static double wrist_setpoint = 0;
+  //Compressor stuff
+  Compressor compressor = new Compressor(9);
   private static DoubleSolenoid shifterDoubleSolenoid = new DoubleSolenoid(9, 7, 3);
   private static DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(9, 0, 6);
-
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
-
+  
+  // Pneumatic wrappers, this is so jank
   public static void drivetrain_shift_high(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kForward); }
   public static void drivetrain_shift_low(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kReverse); }
   public static void intake_close(){ intakeDoubleSolenoid.set(DoubleSolenoid.Value.kForward); }
   public static void intake_open(){ intakeDoubleSolenoid.set(DoubleSolenoid.Value.kReverse); }
+
+  // Set setpoints to 0 at the top
+  // TODO move this to their respective subsystems
+  public static double elevator_setpoint = 0;
+  public static double wrist_setpoint = 0;
+  
+  // Auto stuff
+  // Apparently this has to be defined up here
+  Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
 
 
   /**
@@ -61,19 +69,25 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // setup Operator Input (OI)
     m_oi = new OI();
-    Compressor compressor = new Compressor(9);
-    compressor.setClosedLoopControl(true);
-    
+
+    // Init subsystems
+    // TODO will init be run by default, or do I have to call it?
     drivetrain.init();
     elevator.init();
     wrist.init();
 
-    // m_chooser.addDefault("Default Auto", new ExampleCommand());
-    // chooser.addObject("My Auto", new MyAutoCommand());
-    // SmartDashboard.putString("test", "Hellothere!!! intake clamp: ");
+    // Set compressor mode
+    compressor.setClosedLoopControl(true);
 
+    //Setup gyro
+    ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+		gyro.reset();
 
+    // Auto setup - TODO setup more auto stuff in it's own file
+    m_chooser.addDefault("Default Auto", new auto_action_DRIVE(5, "high", 2, 15));
+    // m_chooser.addObject("My Auto", new MyAutoCommand());
 
   }
 
@@ -90,7 +104,6 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Fore speed axis value", m_oi.getForwardAxis());
     SmartDashboard.putNumber("Turn speed axis value", m_oi.getTurnAxis());
     SmartDashboard.putString("Drivetrain gear", drivetrain.current_gear);
-    // SmartDashboard.putNumber("5 feet per second is this many raw: ", encoderlib.distanceToRaw(5, robotconfig.POSITION_PULSES_PER_ROTATION, robotconfig.left_wheel_effective_diameter));
 
     SmartDashboard.putNumber("Left talon speed", drivetrain.m_left_talon.getSelectedSensorVelocity(0));
     SmartDashboard.putNumber("Left talon error", drivetrain.m_left_talon.getClosedLoopError(0));
@@ -174,39 +187,17 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
-    // new drivetrain_shift_high();
-    // new drivetrain_shift_low();
-    // shifter.set(DoubleSolenoid.Value.kReverse);
-
-
-    // final arcade_drive arcade = new arcade_drive();
-
-    // new arcade_drive();
-
-
   }
 
   /**
    * This function is called periodically during operator control.
-   * 
    */
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
     
-    /**
-     * Update the arcade drivetrain method
-     */
 
-    // new arcade_drive(m_oi.getForwardAxis(), m_oi.getTurnAxis());
-    // drivetrain.m_left_talon.set(ControlMode.PercentOutput, 0.25);
-    // drivetrain.m_right_talon.set(ControlMode.PercentOutput, 0.25);
-
-    // drivetrain_shift_low();
-    // drivetrain_shift_high();
-    
-
+    // TODO replace this with a default command
     double target_intake_speed = m_oi.getIntakeSpeed() / 1;
     // intake.setSpeed(target_intake_speed);
     intake.talon_left.set(ControlMode.PercentOutput, target_intake_speed);
@@ -220,7 +211,7 @@ public class Robot extends TimedRobot {
     // elevator_setpoint = elevator_setpoint+throttle.getRawAxis(1)*10;
     // elevator.setHeight(elevator_setpoint);
 
-    drivetrain.arcadeDriveMethod();
+    // drivetrain.arcadeDriveMethod(m_oi.getForwardAxis(), m_oi.getTurnAxis());
 
   }
 
